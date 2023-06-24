d474e0f94691a4e8197361486867df2ddbd1454f
Prince Mbeah Essilfie <mbeahessilfieprince@gmail.com>
2023-06-25 05:09:36 +1000

    aliases and interfaces with extended interfaces

---
+++ src/alias_interface.ts
@@ -0,0 +1,45 @@
+// aliases allow defining types with a custom name (an Alias)
+
+
+type CarYear = number
+type CarType = string
+type CarModel = string
+type Car = {
+    year: CarYear,
+    type: CarType,
+    model: CarModel
+}
+
+const carYear: CarYear = 2001
+const carType: CarType = "Toyota"
+const carModel: CarModel = "Corolla"
+
+const newCar: Car = {
+    year: carYear,
+    type: carType,
+    model: carModel,
+}
+
+// Interfaces
+// interfaces are just like aliases but only apply to objects
+
+interface Rectangle {
+    height: number,
+    width: number,
+}
+
+const rectangle: Rectangle = {
+    height: 20,
+    width: 10
+}
+
+// Extending Interfaces
+interface ColoredRectangle extends Rectangle {
+    color: string
+}
+
+const coloredRectangle: ColoredRectangle = {
+    height: 20,
+    width: 10,
+    color: "red"
+}
