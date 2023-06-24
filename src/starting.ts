e4ae8872f2ca763e43fd67e9a537362a9afe19a2
Prince Mbeah Essilfie <mbeahessilfieprince@gmail.com>
2023-06-25 03:47:52 +1000

    unknown

--- src/starting.ts
+++ src/starting.ts
@@ -2,4 +2,10 @@ let w: unknown = 1;
 w = "string";
 w = {
     runANonExistentMethod: () => console.log("I think therefor I am")
+} as { runANonExistentMethod: () => void }
+
+if (typeof w === 'object' && w !== null) {
+    (w as {
+        runANonExistentMethod: Function
+    }).runANonExistentMethod();
 }
