26b577baa522acc1524ac49200e9b89c781b0559
Prince Mbeah Essilfie <mbeahessilfieprince@gmail.com>
2023-08-11 22:49:20 +0000

    create a resonating value function

--- learning/resonates.ts
+++ learning/resonates.ts
@@ -2,24 +2,35 @@ function resonateY(numOfPoints: number, height: number) {
    let up = -height * 0.5, down = height * 1.5, counter = height * 0.5, lines: number = 0;
    
    const step = height
-   let moveUp: boolean = true
-   while ( lines <= numOfPoints ) {
-     if (counter > up && moveUp) {
-       console.log(counter)
-       counter -= step;
-     } else if (counter === up && moveUp) {
-        console.log(counter);
-        counter += step;
-       moveUp = false
-     } else if (counter < down && !moveUp) {
-       console.log(counter);
-       counter += step;
-     } else {
-        console.log(counter)
-        counter -= step;
-       moveUp = true
+
+   const controlPointsY = Array.from({ length: numOfPoints }, (_, index) => {
+      let moveUp: boolean = true
+      if (counter > up && moveUp) {
+         let y = counter
+         counter -= step;
+         return y
+         
+      } else if (counter === up && moveUp) {
+         let y = counter;
+         counter += step;
+         moveUp = false
+         return y
+         
+      } else if (counter < down && !moveUp) {
+         let y = counter;
+         counter += step;
+         return y
+         
+      } else {
+         let y = counter;
+         counter -= step;
+         moveUp = true
+         return y
+         
+         }
       }
-      lines++;
-   }
- }
- resonateY(8, 100)
+   )
+   return controlPointsY;
+}
+ 
+console.log(resonateY(8, 100))
